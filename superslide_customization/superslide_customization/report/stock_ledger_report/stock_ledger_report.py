73ae4bb6578421c6d631cf17920dd68fe736fe97
saimore1234 <moresai881@gmai.com>
2026-03-10 23:19:50 +0530

    Seuence

--- superslide_customization/superslide_customization/report/stock_ledger_report/stock_ledger_report.py
+++ superslide_customization/superslide_customization/report/stock_ledger_report/stock_ledger_report.py
@@ -2,34 +2,54 @@ import frappe
 
 
 def execute(filters=None):
+
     filters = frappe._dict(filters or {})
+
     columns = get_columns()
     data = get_data(filters)
+
+    total_qty = 0
+    total_length = 0
+
+    for row in data:
+        total_qty += row.get("qty") or 0
+        total_length += row.get("total_length") or 0
+
+    # Add Grand Total row
+    data.append({
+        "item_name": "<b>Grand Total</b>",
+        "qty": total_qty,
+        "total_length": total_length
+    })
+
     return columns, data
 
 
 def get_columns():
+
     return [
 
-        {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 110},
-        {"label": "Posting Time", "fieldname": "posting_time", "fieldtype": "Time", "width": 90},
+        {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 120},
+
+        {"label": "Posting Time", "fieldname": "posting_time", "fieldtype": "Time", "width": 100},
 
         {"label": "Item Code", "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 150},
+
         {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 200},
 
         {"label": "Length", "fieldname": "length", "fieldtype": "Float", "width": 120},
 
         {"label": "Qty", "fieldname": "qty", "fieldtype": "Float", "width": 100},
 
-        {"label": "Total Length", "fieldname": "total_length", "fieldtype": "Float", "width": 160},
+        {"label": "Total Length", "fieldname": "total_length", "fieldtype": "Float", "width": 150},
 
         {"label": "Warehouse", "fieldname": "warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 180},
 
         {"label": "Balance Qty", "fieldname": "balance_qty", "fieldtype": "Float", "width": 120},
 
-        {"label": "Voucher Type", "fieldname": "voucher_type", "fieldtype": "Data", "width": 140},
+        {"label": "Voucher Type", "fieldname": "voucher_type", "fieldtype": "Data", "width": 150},
 
-        {"label": "Voucher No", "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 160},
+        {"label": "Voucher No", "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 170},
 
         {"label": "Company", "fieldname": "company", "fieldtype": "Link", "options": "Company", "width": 150},
     ]
@@ -40,45 +60,43 @@ def get_data(filters):
     conditions = []
     values = {}
 
-    # -----------------------------------------------------
     # Ledger Type Logic
-    # -----------------------------------------------------
+    if filters.get("ledger_type") == "Full Length Stock Ledger":
 
-    if filters.ledger_type == "Full Length Stock Ledger":
         conditions.append("sle.warehouse = 'FL - TTCPL'")
         length_expr = "item.custom_length"
         total_expr = "item.custom_length * sle.actual_qty"
 
     else:
+
         conditions.append("sle.warehouse = 'Cut Length - TTCPL'")
         length_expr = "MAX(rcp.piece_length)"
         total_expr = "MAX(rcp.piece_length) * ABS(sle.actual_qty)"
 
-    # -----------------------------------------------------
     # Filters
-    # -----------------------------------------------------
 
-    if filters.company:
+    if filters.get("company"):
         conditions.append("sle.company = %(company)s")
-        values["company"] = filters.company
+        values["company"] = filters.get("company")
 
-    if filters.item_code:
+    if filters.get("item_code"):
         conditions.append("sle.item_code = %(item_code)s")
-        values["item_code"] = filters.item_code
+        values["item_code"] = filters.get("item_code")
 
-    if filters.warehouse:
+    if filters.get("warehouse"):
         conditions.append("sle.warehouse = %(warehouse)s")
-        values["warehouse"] = filters.warehouse
+        values["warehouse"] = filters.get("warehouse")
 
-    if filters.from_date:
+    if filters.get("from_date"):
         conditions.append("sle.posting_date >= %(from_date)s")
-        values["from_date"] = filters.from_date
+        values["from_date"] = filters.get("from_date")
 
-    if filters.to_date:
+    if filters.get("to_date"):
         conditions.append("sle.posting_date <= %(to_date)s")
-        values["to_date"] = filters.to_date
+        values["to_date"] = filters.get("to_date")
 
     condition_sql = " AND ".join(conditions)
+
     if condition_sql:
         condition_sql = "AND " + condition_sql
 
