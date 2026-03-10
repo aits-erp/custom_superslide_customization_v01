0465015e2d6d2a0949f14216f6be780680f4ac76
saimore1234 <moresai881@gmai.com>
2026-03-10 19:31:19 +0530

    Seuence

---
+++ superslide_customization/superslide_customization/report/stock_ledger_report/stock_ledger_report.js
@@ -0,0 +1,46 @@
+frappe.query_reports["Stock Ledger Report"] = {
+    "filters": [
+
+        {
+            "fieldname": "company",
+            "label": "Company",
+            "fieldtype": "Link",
+            "options": "Company",
+            "reqd": 1
+        },
+
+        {
+            "fieldname": "from_date",
+            "label": "From Date",
+            "fieldtype": "Date"
+        },
+
+        {
+            "fieldname": "to_date",
+            "label": "To Date",
+            "fieldtype": "Date"
+        },
+
+        {
+            "fieldname": "item_code",
+            "label": "Item Code",
+            "fieldtype": "Link",
+            "options": "Item"
+        },
+
+        {
+            "fieldname": "warehouse",
+            "label": "Warehouse",
+            "fieldtype": "Link",
+            "options": "Warehouse"
+        },
+
+        {
+            "fieldname": "voucher_type",
+            "label": "Voucher Type",
+            "fieldtype": "Select",
+            "options": "\nStock Entry\nPurchase Receipt\nDelivery Note\nStock Reconciliation"
+        }
+
+    ]
+};
---
+++ superslide_customization/superslide_customization/report/stock_ledger_report/stock_ledger_report.py
@@ -0,0 +1,76 @@
+import frappe
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+
+def get_columns():
+    return [
+        {"label": "Posting Date", "fieldname": "posting_date", "fieldtype": "Date", "width": 120},
+        {"label": "Item Code", "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 150},
+        {"label": "Item Name", "fieldname": "item_name", "fieldtype": "Data", "width": 180},
+        {"label": "Custom Length", "fieldname": "custom_length", "fieldtype": "Float", "width": 120},
+        {"label": "Warehouse", "fieldname": "warehouse", "fieldtype": "Link", "options": "Warehouse", "width": 180},
+        {"label": "Actual Qty", "fieldname": "actual_qty", "fieldtype": "Float", "width": 120},
+        {"label": "Balance Qty", "fieldname": "qty_after_transaction", "fieldtype": "Float", "width": 150},
+        {"label": "Voucher Type", "fieldname": "voucher_type", "fieldtype": "Data", "width": 140},
+        {"label": "Voucher No", "fieldname": "voucher_no", "fieldtype": "Dynamic Link", "options": "voucher_type", "width": 160},
+        {"label": "Company", "fieldname": "company", "fieldtype": "Link", "options": "Company", "width": 150},
+    ]
+
+
+def get_data(filters):
+
+    conditions = ""
+    values = {}
+
+    if filters.get("item_code"):
+        conditions += " AND sle.item_code = %(item_code)s"
+        values["item_code"] = filters.get("item_code")
+
+    if filters.get("warehouse"):
+        conditions += " AND sle.warehouse = %(warehouse)s"
+        values["warehouse"] = filters.get("warehouse")
+
+    if filters.get("voucher_type"):
+        conditions += " AND sle.voucher_type = %(voucher_type)s"
+        values["voucher_type"] = filters.get("voucher_type")
+
+    if filters.get("company"):
+        conditions += " AND sle.company = %(company)s"
+        values["company"] = filters.get("company")
+
+    if filters.get("from_date"):
+        conditions += " AND sle.posting_date >= %(from_date)s"
+        values["from_date"] = filters.get("from_date")
+
+    if filters.get("to_date"):
+        conditions += " AND sle.posting_date <= %(to_date)s"
+        values["to_date"] = filters.get("to_date")
+
+    data = frappe.db.sql(f"""
+    SELECT
+        sle.posting_date,
+        sle.item_code,
+        item.item_name,
+        item.custom_length,
+        sle.warehouse,
+        sle.actual_qty,
+        sle.qty_after_transaction,
+        sle.voucher_type,
+        sle.voucher_no,
+        sle.company
+    FROM
+        `tabStock Ledger Entry` sle
+    LEFT JOIN
+        `tabItem` item
+    ON
+        sle.item_code = item.name
+    WHERE
+        sle.docstatus < 2
+        {conditions}
+    ORDER BY
+        sle.posting_date DESC, sle.posting_time DESC
+""", values, as_dict=1)
